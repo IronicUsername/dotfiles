6413b82ee5b05377ebc378c7f863b1911ca003eb
IronicUsername <github@trash-economy.io>
2022-04-02 15:05:09 +0200

    adjust

--- _pythonconfig/python.sh
+++ _pythonconfig/python.sh
@@ -12,7 +12,8 @@ mkdir -p $XDG_CONFIG_HOME/zsh/custom/plugins/poetry
 poetry completions zsh > $XDG_CONFIG_HOME/zsh/custom/plugins/poetry/_poetry
 
 echo "Installing python, rust & node..."
-. /opt/homebrew/opt/asdf/libexec/asdf.sh
+. /opt/homebrew/opt/asdf/libexec/asdf.sh  # macos 12.3
+. /usr/local/opt/asdf/libexec/asdf.sh # macos <12.3
 export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME}/asdf/asdfrc"
 export ASDF_DATA_DIR="${XDG_DATA_HOME}/asdf"
 
